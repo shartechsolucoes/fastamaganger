5c984aeb611b0adfb6f998c40526c46095780b78
lucianolecky <90874643+lucianolecky@users.noreply.github.com>
2025-07-11 21:08:37 -0300

    Mudei nos Planos

--- src/routes/PublicRoutes.ts
+++ src/routes/PublicRoutes.ts
@@ -1,9 +1,12 @@
 import LandingPage from '../pages/LandingPage';
+import LandingPrices from '../pages/LandingPage/pricing';
 import Login from '../pages/Login';
 import SignIn from '../pages/SignIn';
 
 export const publicRoutes = [
 	{ name: 'FastMananger', path: '/landing', component: LandingPage},
+	{ name: 'prices', path: '/prices', component: LandingPrices},
 	{ name: 'login', path: '/login', component: Login },
 	{ name: 'signIn', path: '/sign-in', component: SignIn },
+
 ];

eee0e557db478a1d2f3e6a8cd2fe1da6e25fe67a
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-07-18 15:30:15 -0300

    adicionado dados fakes traking

--- src/pages/Tracking/index.tsx
+++ src/pages/Tracking/index.tsx
@@ -8,32 +8,84 @@ export default function Traking() {
 	return (
 		<div className="card">
 			<div className="card-body">
-				<div className="mb-3 col-12">
-					<label htmlFor="serviceType" className="form-label">
-						Pesquisar protocolo
-					</label>
-					<div className="d-flex gap-2">
-						<input
-							type="text"
-							id="serviceType"
-							className="form-control"
-							// value={serviceType}
-							required
-							// onChange={(e) => onChange('serviceType', e.target.value)}
-						/>
-						<button type="button" className="btn btn-primary">
-							<MdOutlineSearch />
-						</button>
+				<div className="row">
+					<div className="mb-3 col-12">
+						<label htmlFor="serviceType" className="form-label">
+							Pesquisar protocolo
+						</label>
+						<div className="d-flex gap-2">
+							<input
+								type="text"
+								id="serviceType"
+								className="form-control"
+								// value={serviceType}
+								required
+								// onChange={(e) => onChange('serviceType', e.target.value)}
+							/>
+							<button type="button" className="btn btn-primary">
+								<MdOutlineSearch />
+							</button>
+						</div>
+					</div>
+					<div className="mb-3 col-12">
+						<StepProgress currentStep={4} steps={STEPS} />
+					</div>
+
+					<div className="mb-3 col-12 ">
+						<p className="form-label fs-3">Detalhes</p>
+					</div>
+					<hr />
+					<div className="mb-3 col-12 fs-4">
+						<p className="form-label fs-4">Local</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">CEP</label>
+						<p>12345-678</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Endereço</label>
+						<p>Rua das Flores</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Número</label>
+						<p>987</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Bairro</label>
+						<p>Jardim Primavera</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Cidade</label>
+						<p>São Paulo</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Estado</label>
+						<p>SP</p>
+					</div>
+					<div className="mb-3 col-12 fs-4">
+						<p className="form-label fs-4">Serviço prestado</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Tipo de Serviço</label>
+						<p>Troca de Lâmpada</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Status</label>
+						<p>Finalizado</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Data Início</label>
+						<p>16/07/2025 08:30</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Data Finalizado</label>
+						<p>16/07/2025 09:15</p>
+					</div>
+					<div className="mb-3 col-12 col-md-6">
+						<label className="form-label">Tempo (h)</label>
+						<p>0.75</p>
 					</div>
 				</div>
-				<StepProgress currentStep={2} steps={STEPS} />
-
-				<div className="mb-3 col-12">
-					<label htmlFor="serviceType" className="form-label">
-						Detalhes
-					</label>
-				</div>
-				<hr />
 			</div>
 		</div>
 	);
